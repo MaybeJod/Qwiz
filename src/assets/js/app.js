ff2597f3a74613debdbd534c021d9f0b6c3d2979
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2024-12-21 15:16:48 +0100

    fix: added imports to app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,20 @@
+/* styles */
 import "../styles/reset.css";
 import "../styles/style.css";
 
-export function app() {}
+/* js */
+import fetchApi from "./fetchApi";
+import getQuestions from "./getQuestions";
+
+export function app() {
+	const buttonElement = document.getElementById("newQuestionButton");
+
+	buttonElement.addEventListener("click", () => {
+		localStorage.clear();
+		const ApiUrl = "https://opentdb.com/api.php?amount=3&type=multiple";
+		fetchApi(ApiUrl);
+		getQuestions();
+	});
+
+	getQuestions();
+}
